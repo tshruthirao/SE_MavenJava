8b063791dc280e0f852f918e29d50f4dffcddff4
tshruthirao <tshruthirao16@gmail.com>
2024-12-03 22:39:52 +0530

    Update App.java

--- src/main/java/com/maven/project1/my_maven_project/App.java
+++ src/main/java/com/maven/project1/my_maven_project/App.java
@@ -5,6 +5,6 @@ package com.maven.project1.my_maven_project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Java Project");
+        System.out.println("Hello Java !");
     }
 }
